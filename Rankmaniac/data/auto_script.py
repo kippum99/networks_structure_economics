ebbb684af6e67bfc599e8735aad8f67c072c21d3
June Kim <jooeun@caltech.edu>
2020-02-12 22:33:59 -0800

    Implemented early stopping

--- Rankmaniac/data/auto_script.py
+++ Rankmaniac/data/auto_script.py
@@ -1,8 +1,15 @@
 import os
+import sys
 
-test_file = '../local_test_data/GNPn100p05.txt'
-# test_file = '../local_test_data/EmailEnron.txt'
-# test_file = '../local_test_data/Gnutella.txt'
+if sys.argv[1] == '1':
+    test_file = '../local_test_data/GNPn100p05.txt'
+elif sys.argv[1] == '2':
+    test_file = '../local_test_data/EmailEnron.txt'
+elif sys.argv[1] == '3':
+    test_file = '../local_test_data/Gnutella.txt'
+else:
+    test_file = sys.argv[1]
+
 
 os.system('python2 pagerank_map.py < ' + test_file + ' | sort | \
             python2 pagerank_reduce.py | python2 process_map.py | sort | \
--- Rankmaniac/data/process_reduce.py
+++ Rankmaniac/data/process_reduce.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import sys
 from heapq import nlargest
 
@@ -8,8 +9,14 @@ def emit(key, value):
     sys.stdout.write(str(key) + '\t' + str(value) + '\n')
 
 
+EPSILON = 0.001
+
+
 first_iter = True
 last_iter = False
+num_nodes = 0
+iter_num = 0
+squared_difference = 0
 
 # Copy of stdin
 lst_outputs = []
@@ -24,8 +31,6 @@ for line in sys.stdin:
         if iter_num == 50:
             # Only print top 20 (should we do it in process_map?)
             last_iter = True
-        else:
-            emit('Iter:', iter_num)
     else:
         # For iterations < 50
         lst_outputs.append(line)
@@ -34,10 +39,13 @@ for line in sys.stdin:
         node_id, info = line.strip('\n').split('\t')
         node_id = node_id[len('NodeId:'):]
         info = info.split(',')
-        rank = info[0]
+        rank, prev_rank  = info[0:2]
         ranks.append((rank, node_id))
 
-if last_iter:
+        num_nodes += 1
+        squared_difference += (float(rank) - float(prev_rank)) ** 2
+
+if last_iter or math.sqrt(squared_difference / num_nodes) < EPSILON:
     top_ranks = nlargest(20, ranks)
     top_ranks.sort(reverse=True)
     # top_ranks = sorted(ranks, reverse=True)[:20]
@@ -47,6 +55,8 @@ if last_iter:
 else:
     if first_iter:
         emit('Iter:', 1)
+    else:
+        emit('Iter:', iter_num)
 
     for output in lst_outputs:
         sys.stdout.write(output)
