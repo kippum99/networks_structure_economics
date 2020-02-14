e884cee73887069531fff70861b2cc8fe83df1b4
June Kim <jooeun@caltech.edu>
2020-02-13 22:15:41 -0800

    Added tolerance for ordering convergence

--- Rankmaniac/data/process_reduce.py
+++ Rankmaniac/data/process_reduce.py
@@ -8,6 +8,8 @@ from heapq import nlargest
 def emit(key, value):
     sys.stdout.write(str(key) + '\t' + str(value) + '\n')
 
+# Threshold for max squared diff in ordering (convergence testing)
+penalty_tolerance = 20
 
 first_iter = True
 last_iter = False
@@ -19,9 +21,7 @@ lst_outputs = []
 
 # List of tuples (rank, node_id)
 ranks = []
-
-# Dictionary mapping node to prev_rank
-prev_ranks = {}
+prev_ranks = []
 
 for line in sys.stdin:
     if 'Iter:' in line:
@@ -39,28 +39,48 @@ for line in sys.stdin:
         info = info.split(',')
         rank, prev_rank  = info[0:2]
         ranks.append((rank, node_id))
+        prev_ranks.append((prev_rank, node_id))
 
         num_nodes += 1
-        prev_ranks[node_id] = prev_rank
 
 # Check convergence (relative ordering of top 20)
 converged = False
 
 if not first_iter:
-    top_ranks = nlargest(20, ranks)
+    top_ranks = nlargest(25, ranks)
     top_ranks.sort(reverse=True)
 
-    prev_rank_compare = sys.maxsize
-    node_id_compare = -1
+    # Top 25 previous ranks
+    top_prev_ranks = nlargest(25, prev_ranks)
+    top_prev_nodes_set = set(pair[1] for pair in top_prev_ranks)
+
     converged = True
 
+    # Check if top 20 of current ranks are within top 25 of prev ranks
     for _, node_id in top_ranks:
-        prev_rank = float(prev_ranks[node_id])
-        if prev_rank > prev_rank_compare:
+        if node_id not in top_prev_nodes_set:
             converged = False
             break
-        prev_rank_compare = prev_rank
-        node_id_compare = node_id
+
+    # If all top 20 of current ranks are within top 25 of prev ranks,
+    # compute squared difference in ordering
+    if converged:
+        top_prev_ranks.sort(reverse=True)
+
+        # Dictionary mapping node_id to prev_ordering
+        prev_ordering = {}
+
+        for i, (_, node_id) in enumerate(top_prev_ranks):
+            prev_ordering[node_id] = i
+
+        # Compute squared difference in ordering
+        sum_squared_diff = 0
+
+        for i, (_, node_id) in enumerate(top_ranks):
+            diff = i - prev_ordering[node_id]
+            sum_squared_diff += diff ** 2
+
+        converged = sum_squared_diff <= penalty_tolerance
 
 if last_iter or converged:
     for rank, node_id in top_ranks:
