b6d2eae221d7c4d68e43cbaa5b620f0ae48f8aa3
June Kim <jooeun@caltech.edu>
2020-02-13 17:25:27 -0800

    Implemented early stopping on top 20 relative ordering convergence; Added termination on final ouptut for auto_script

--- Rankmaniac/data/auto_script.py
+++ Rankmaniac/data/auto_script.py
@@ -16,7 +16,10 @@ os.system('python2 pagerank_map.py < ' + test_file + ' | sort | \
             python2 process_reduce.py > outputs/output1.txt')
 
 for i in range(2, 51):
-    os.system('python2 pagerank_map.py < outputs/output' + str(i - 1)
-                + '.txt | sort | \
+    prev_output = 'outputs/output' + str(i - 1) + '.txt'
+    with open(prev_output, 'r') as f:
+        if 'FinalRank' in f.readline():
+            break
+    os.system('python2 pagerank_map.py < ' + prev_output + ' | sort | \
                 python2 pagerank_reduce.py | python2 process_map.py | sort | \
                 python2 process_reduce.py > outputs/output' + str(i) + '.txt')
--- Rankmaniac/data/process_reduce.py
+++ Rankmaniac/data/process_reduce.py
@@ -9,14 +9,14 @@ def emit(key, value):
     sys.stdout.write(str(key) + '\t' + str(value) + '\n')
 
 
-EPSILON = 0.001
+# EPSILON = 0.007
 
 
 first_iter = True
 last_iter = False
 num_nodes = 0
 iter_num = 0
-squared_difference = 0
+# squared_difference = 0
 
 # Copy of stdin
 lst_outputs = []
@@ -24,12 +24,14 @@ lst_outputs = []
 # List of tuples (rank, node_id)
 ranks = []
 
+# Dictionary mapping node to prev_rank
+prev_ranks = {}
+
 for line in sys.stdin:
     if 'Iter:' in line:
         first_iter = False
         iter_num = int(line.split('\t')[1]) + 1
         if iter_num == 50:
-            # Only print top 20 (should we do it in process_map?)
             last_iter = True
     else:
         # For iterations < 50
@@ -43,13 +45,37 @@ for line in sys.stdin:
         ranks.append((rank, node_id))
 
         num_nodes += 1
-        squared_difference += (float(rank) - float(prev_rank)) ** 2
+        prev_ranks[node_id] = prev_rank
+        # squared_difference += (float(rank) - float(prev_rank)) ** 2
 
-if last_iter or math.sqrt(squared_difference / num_nodes) < EPSILON:
-    top_ranks = nlargest(20, ranks)
-    top_ranks.sort(reverse=True)
-    # top_ranks = sorted(ranks, reverse=True)[:20]
+# Check convergence (relative ordering of top 20)
+top_ranks = nlargest(20, ranks)
+top_ranks.sort(reverse=True)
 
+converged = False
+prev_rank_compare = sys.maxsize
+node_id_compare = -1
+
+# # Debugging
+# f = open("debug/log" + str(iter_num) + ".txt", "w")
+
+if not first_iter:
+    converged = True
+    for _, node_id in top_ranks:
+        prev_rank = float(prev_ranks[node_id])
+        if prev_rank > prev_rank_compare:
+            # f.write(prev_rank + ' for node ' + node_id + ' > ' + prev_rank_compare + ' for node ' + node_id_compare + '\n')
+            converged = False
+            break
+        prev_rank_compare = prev_rank
+        node_id_compare = node_id
+
+
+# for rank, node_id in top_ranks:
+#     f.write(str(rank) + ' ' + str(node_id) + '\n')
+# f.close()
+
+if last_iter or converged:
     for rank, node_id in top_ranks:
         emit('FinalRank:' + rank, node_id)
 else:
